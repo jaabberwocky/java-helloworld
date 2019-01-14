d7d74f46fa706399eaf41c51315265476b15c3b5
tobias <tobiasleongzhunmun@gmail.com>
2019-01-15 00:03:11 +0800

    added a bunch of new string methods

--- src/StringMain.java
+++ src/StringMain.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class StringMain {
 
     public static void main(String[] args){
-        String userinput = "entertainment";
+
+        // give Scanner the input from System.in
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter word or phrase then press enter:");
+        String userinput = sc.nextLine();
+
         String uppercase = userinput.toUpperCase();
-        System.out.println(uppercase);
+        System.out.println("Uppercased! " + uppercase);
 
         // access the first character
         char firstCharacter = userinput.charAt(0);
-
-        System.out.println(firstCharacter);
+        System.out.println("First character: " + firstCharacter);
 
         // see if string contains something
         System.out.println("Contains \"input\"? " + userinput.contains("input"));
+
+        // output length of phrase
+        System.out.println("Length: " + userinput.length());
+
+        // take int input
+        System.out.println("Enter a number:");
+        int userNumber = sc.nextInt();
+        System.out.println("Entered: " + userNumber);
     }
 }
