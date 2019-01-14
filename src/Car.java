af917b7a8267e8c7911a7e677ded41865046c5b9
tobias <tobiasleongzhunmun@gmail.com>
2019-01-14 22:07:56 +0800

    add car classes

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public class Car {
+
+    // Data Types:
+    // int -> integers
+    // double -> decimal values
+    // Java has a Color class, but need to import first from java.awt library (abstract window toolkit)
+    // boolean -> true or false
+
+    double averageMilesPerGallon;
+    String licensePlate;
+    Color paintColor;
+    boolean areTailightsWorking;
+
+    // create the constructor class
+    // this refers to the car we are creating
+    public Car(double inputAverageMPG,
+               String inputLicensePlate,
+               Color inputPaintColor,
+               boolean inputAreTailightsWorking) {
+        this.averageMilesPerGallon = inputAverageMPG;
+        this.licensePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.areTailightsWorking = inputAreTailightsWorking;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from IntelliJ!");
+        System.out.println("Tobias!!");
+
+        // note you dont have to import as java imposes class = name of file
+        // so it knows where to look
+        Car myCar = new Car(25.5, "1BC32E", Color.BLUE, true);
+        Car sallyCar = new Car(13.9, "3D20BN", Color.BLACK, false);
+
+        System.out.println("My car's license plate: " + myCar.licensePlate);
+        System.out.println("Sally's car's license plate: " + sallyCar.licensePlate);
+    }
+}
