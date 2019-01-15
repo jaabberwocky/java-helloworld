76ed923f819cf071be42c2b7a9ae71b6e4437343
tobias <tobiasleongzhunmun@gmail.com>
2019-01-15 22:49:02 +0800

    add arrays

---
+++ src/ArrayTutorial.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ArrayTutorial {
+
+    public static void main(String[] args) {
+        // arrays' size cant be changed once created
+
+        int[] numbers = {31,45,22,98,10};
+
+        // pretty prints the java array
+        System.out.println(Arrays.toString(numbers));
+
+        // sort the array now
+        int[] sorted = Arrays.copyOf(numbers, numbers.length);
+        Arrays.sort(sorted);
+        System.out.println("Sorted!");
+        System.out.println(Arrays.toString(sorted));
+    }
+}
